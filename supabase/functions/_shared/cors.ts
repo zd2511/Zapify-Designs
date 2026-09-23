fb86e160268582f70fdac14c326538beda497ed5
zd2511 <zapifydesigns@gmail.com>
2026-09-23 10:18:58 +0200

    Delete supabase directory

--- supabase/functions/_shared/cors.ts
+++
@@ -1,5 +0,0 @@
-export const corsHeaders = {
-  'Access-Control-Allow-Origin': Deno.env.get('CORS_ORIGIN') || '*',
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type, idempotency-key',
-  'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
-};
